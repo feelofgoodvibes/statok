8cd50454672ed350fc5d2e617d46b96796d96e2d
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-02-15 05:06:01 +0200

    Add operations list to index.html

--- documentation/html_prototype/js/scripts.js
+++ documentation/html_prototype/js/scripts.js
@@ -2,12 +2,21 @@ window.addEventListener("load", () => {
     let logoel = document.getElementById("logo");
 
     if (logoel){
-        logoel.addEventListener("click", () => { location.href = "/"; })
+        logoel.addEventListener("click", () => { location.href = "./index.html"; })
     }
 
     $("#btn-filters").accordion({
         active: false,
         collapsible: true
+    });
+
+    $("#jquery-del-dialog").dialog({
+        autoOpen: false,
+        buttons: [
+            {
+                text: "Yes, delete"
+            }
+        ]
     });
 
     $('input[name="daterange"]').daterangepicker({
@@ -18,8 +27,36 @@ window.addEventListener("load", () => {
             format: "DD/MM/YYYY hh:mm"
         }
     });
+
+    // Render total money
+    let total = 0;
+    operations_data.forEach(operation => {
+        total += operation.value
+    });
+
+    if (total >= 0){
+        total_text = "$" + total;
+        $("#total-money-value").addClass("money-pos");
+    }
+    else {
+        total_text = "-$" + Math.abs(total);
+        $("#total-money-value").addClass("money-neg");
+    }
+
+    $("#total-money-value").text(total_text);
 });
 
 function formClick() {
     console.log($("#filter-daterange").val());
+}
+
+function delF(element) {
+    console.log(element);
+
+    swal({
+            text: "Are you sure you want to delete this item?",
+            buttons: ["Cancel", "Yes"],
+            icon: "warning",
+            dangerMode: true
+        });
 }
