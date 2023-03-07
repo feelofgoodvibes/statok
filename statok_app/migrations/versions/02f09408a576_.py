b4fd011299292c4d12e4b26be52f46e0f44da8bb
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-03-07 06:23:38 +0200

    Add more data to test data migration

--- statok_app/migrations/versions/02f09408a576_.py
+++ statok_app/migrations/versions/02f09408a576_.py
@@ -1,4 +1,4 @@
-"""empty message
+"""Test data
 
 Revision ID: 02f09408a576
 Revises: baabe4b7303f
@@ -29,18 +29,32 @@ def upgrade():
     op.execute("INSERT INTO category VALUES (9, 'Health', 'EXPENSE')")
 
     # Operations
-    op.execute("INSERT INTO operation VALUES (1, 2000, '2023-03-01 15:00:00', 3)")
-    op.execute("INSERT INTO operation VALUES (2, -12.56, '2023-03-01 16:18:12', 7)")
-    op.execute("INSERT INTO operation VALUES (3, -135, '2023-03-01 16:55:13', 8)")
-    op.execute("INSERT INTO operation VALUES (4, -5.45, '2023-03-01 16:55:13', 9)")
-    op.execute("INSERT INTO operation VALUES (5, -13.5, '2023-03-02 10:02:10', 9)")
-    op.execute("INSERT INTO operation VALUES (6, -26.88, '2023-03-02 13:46:05', 6)")
-    op.execute("INSERT INTO operation VALUES (7, 139, '2023-03-02 15:12:00', 1)")
-    op.execute("INSERT INTO operation VALUES (8, 250, '2023-03-02 16:20:06', 1)")
-    op.execute("INSERT INTO operation VALUES (9, -62.25, '2023-03-02 18:02:12', 7)")
-    op.execute("INSERT INTO operation VALUES (10, -13.56, '2023-03-02 18:25:06', 6)")
-    op.execute("INSERT INTO operation VALUES (11, -26.8, '2023-03-02 18:33:46', 6)")
-    op.execute("INSERT INTO operation VALUES (12, -35.89, '2023-03-02 18:39:55', 6)")
+    op.execute("INSERT INTO operation VALUES (1, 2000, '2023-02-01 15:00:00', 3)")
+    op.execute("INSERT INTO operation VALUES (2, -12.56, '2023-02-01 16:18:12', 7)")
+    op.execute("INSERT INTO operation VALUES (3, -135, '2023-02-01 16:55:13', 8)")
+    op.execute("INSERT INTO operation VALUES (4, -5.45, '2023-02-01 16:55:13', 9)")
+
+    op.execute("INSERT INTO operation VALUES (5, -13.5, '2023-02-02 10:02:10', 9)")
+    op.execute("INSERT INTO operation VALUES (6, -26.88, '2023-02-02 13:46:05', 6)")
+    op.execute("INSERT INTO operation VALUES (7, 139, '2023-02-02 15:12:00', 1)")
+    op.execute("INSERT INTO operation VALUES (8, 250, '2023-02-02 16:20:06', 1)")
+    op.execute("INSERT INTO operation VALUES (9, -62.25, '2023-02-02 18:02:12', 7)")
+    op.execute("INSERT INTO operation VALUES (10, -13.56, '2023-02-02 18:25:06', 6)")
+    op.execute("INSERT INTO operation VALUES (11, -26.8, '2023-02-02 18:33:46', 6)")
+    op.execute("INSERT INTO operation VALUES (12, -35.89, '2023-02-02 18:39:55', 6)")
+
+    op.execute("INSERT INTO operation VALUES (13, -12.00, '2023-02-03 10:05:12', 7)")
+    op.execute("INSERT INTO operation VALUES (14, -5.00, '2023-02-03 13:05:13', 9)")
+    op.execute("INSERT INTO operation VALUES (15, -1.23, '2023-02-03 21:13:50', 5)")
+
+    op.execute("INSERT INTO operation VALUES (16, -120.13, '2023-02-04 13:00:15', 8)")
+    op.execute("INSERT INTO operation VALUES (17, -6.89, '2023-02-04 23:15:10', 6)")
+
+    op.execute("INSERT INTO operation VALUES (18, -240.00, '2023-02-05 09:12:18', 5)")
+
+    op.execute("INSERT INTO operation VALUES (19, 500.00, '2023-02-10 08:00:00', 3)")
+    op.execute("INSERT INTO operation VALUES (20, -260.50, '2023-02-10 16:43:19', 8)")
+    op.execute("INSERT INTO operation VALUES (21, -125.12, '2023-02-10 21:02:15', 6)")
 
 
 def downgrade():
