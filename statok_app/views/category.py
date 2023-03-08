e98ce06f62b5e638217d67018745ea3ba067d392
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-03-08 05:14:07 +0200

    Change API url getting method inside view functions to universal

--- statok_app/views/category.py
+++ statok_app/views/category.py
@@ -1,13 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 import requests
-
-API_URL = "http://localhost:5000/api/v1"
 
 
 def view_categories():
     """View for URL: /category"""
 
-    categories = requests.get(f"{API_URL}/category/stats", timeout=5).json()
+    categories = requests.get(f"{request.host_url}/api/v1/category/stats", timeout=5).json()
 
     return render_template("categories.html", categories=categories)
 
@@ -15,6 +13,6 @@ def view_categories():
 def view_category(category_id: int):
     """View for URL: /category/<int:category_id>"""
 
-    category = requests.get(f"{API_URL}/category/{category_id}", timeout=5).json()
+    category = requests.get(f"{request.host_url}/api/v1/category/{category_id}", timeout=5).json()
 
     return render_template("category.html", category=category)
--- statok_app/views/operation.py
+++ statok_app/views/operation.py
@@ -1,18 +1,16 @@
 from flask import render_template, request
 import requests
 
-API_URL = "http://localhost:5000/api/v1"
-
 
 def view_operations():
     """View for URL: /operation"""
 
     # Get operation with filters, retrieved form request URL
-    operations = requests.get(f"{API_URL}/operation", params=request.args, timeout=5).json()
+    operations = requests.get(f"{request.host_url}/api/v1/operation", params=request.args, timeout=5).json()
 
     # If filters in the URL are wrong - get all operations without filters (default behaviour)
     if "error" in operations:
-        operations = requests.get(f"{API_URL}/operation", timeout=5).json()
+        operations = requests.get(f"{request.host_url}/api/v1/operation", timeout=5).json()
 
     return render_template("operations.html", operations=operations)
 
@@ -20,6 +18,6 @@ def view_operations():
 def view_operation(operation_id: int):
     """View for URL: /operation/<int:operation_id>"""
 
-    operation = requests.get(f"{API_URL}/operation/{operation_id}", timeout=5).json()
+    operation = requests.get(f"{request.host_url}/api/v1/operation/{operation_id}", timeout=5).json()
 
     return render_template("operation.html", operation=operation)
